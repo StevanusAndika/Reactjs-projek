cd679248eb01ea9f5132432d47cc59f8a1b7eda6
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-08 02:24:32 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css'
-
+import App from './public/img/paper-bg.jpg'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
